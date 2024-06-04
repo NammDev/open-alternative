f0d29e7ea25807238b49fbbc214d462343116e35
Nam <namdeveloper.ca@gmail.com>
2024-06-04 11:41:57 +0700

    style header

---
+++ src/components/app-ui/Badge.tsx
@@ -0,0 +1,36 @@
+import { Slot } from "@radix-ui/react-slot";
+import { type HTMLAttributes, isValidElement } from "react";
+import { type VariantProps, cva, cx } from "@/lib/cva";
+
+export const badgeVariants = cva({
+  base: "rounded bg-border/50 text-secondary hover:[&[href]]:bg-border/75",
+
+  variants: {
+    size: {
+      sm: "px-1 py-px text-[10px]/tight",
+      md: "px-1.5 py-0.5 text-xs/tight",
+    },
+  },
+
+  defaultVariants: {
+    size: "md",
+  },
+});
+
+type BadgeProps = HTMLAttributes<HTMLElement> &
+  VariantProps<typeof badgeVariants> & {
+    /**
+     * If set to `true`, the button will be rendered as a child within the component.
+     * This child component must be a valid React component.
+     */
+    asChild?: boolean;
+  };
+
+export const Badge = ({ className, asChild, size, ...props }: BadgeProps) => {
+  const useAsChild = asChild && isValidElement(props.children);
+  const Component = useAsChild ? Slot : "span";
+
+  return (
+    <Component className={cx(badgeVariants({ size, className }))} {...props} />
+  );
+};
--- src/components/layouts/header.tsx
+++ src/components/layouts/header.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 
 import {
   BlocksIcon,
@@ -12,135 +12,214 @@ import {
   SmilePlusIcon,
   TagIcon,
   XIcon,
-} from 'lucide-react'
-import { cn } from '@/lib/utils'
-import { useEffect, useState } from 'react'
-import { Breadcrumb } from '../ui/breadcrumb'
+} from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { Breadcrumb } from "../ui/breadcrumb";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
-} from '../ui/dropdown-menu'
-import Link from 'next/link'
+} from "../ui/dropdown-menu";
+import Link from "next/link";
+import { GITHUB_URL } from "@/lib/constants";
+import { Badge } from "../app-ui/Badge";
+import { Series } from "../app-ui/Series";
+import { Button } from "../app-ui/Button";
+import { Ping } from "../app-ui/Ping";
 
 export const Header = () => {
-  const [isNavOpen, setNavOpen] = useState(false)
-  const formatter = new Intl.NumberFormat('en-US', { notation: 'compact' })
+  const [isNavOpen, setNavOpen] = useState(false);
+  const formatter = new Intl.NumberFormat("en-US", { notation: "compact" });
 
   // Close the mobile navigation when the user presses the "Escape" key
   useEffect(() => {
     const onKeyDown = (e: KeyboardEvent) => {
-      if (e.key === 'Escape') setNavOpen(false)
-    }
+      if (e.key === "Escape") setNavOpen(false);
+    };
 
-    document.addEventListener('keydown', onKeyDown)
-    return () => document.removeEventListener('keydown', onKeyDown)
-  }, [])
+    document.addEventListener("keydown", onKeyDown);
+    return () => document.removeEventListener("keydown", onKeyDown);
+  }, []);
 
   return (
     <div
       className={cn(
-        'sticky top-0 z-10 flex flex-wrap items-center py-3 -my-3 gap-3 backdrop-blur-sm bg-background/95 md:gap-4'
+        "sticky top-0 z-10 -my-3 flex flex-wrap items-center gap-3 bg-background/95 py-3 backdrop-blur-sm md:gap-4",
       )}
     >
-      <button type='button' onClick={() => setNavOpen(!isNavOpen)} className='lg:hidden'>
-        <MenuIcon className={cn('size-6 stroke-[1.5]', isNavOpen && 'hidden')} />
-        <XIcon className={cn('size-6 stroke-[1.5]', !isNavOpen && 'hidden')} />
-        <span className='sr-only'>Toggle navigation</span>
+      <button
+        type="button"
+        onClick={() => setNavOpen(!isNavOpen)}
+        className="lg:hidden"
+      >
+        <MenuIcon
+          className={cn("size-6 stroke-[1.5]", isNavOpen && "hidden")}
+        />
+        <XIcon className={cn("size-6 stroke-[1.5]", !isNavOpen && "hidden")} />
+        <span className="sr-only">Toggle navigation</span>
       </button>
 
-      <Breadcrumb className='mr-auto' />
+      <Breadcrumb className="mr-auto" />
 
-      <nav className='contents max-lg:hidden'>
+      <nav className="contents max-lg:hidden">
         <DropdownMenu>
-          <DropdownMenuTrigger className='gap-1'>
+          <DropdownMenuTrigger className="group -m-0.5 flex cursor-pointer items-center gap-1 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50">
             Browse <ChevronDownIcon />
           </DropdownMenuTrigger>
 
-          <DropdownMenuContent align='end'>
+          <DropdownMenuContent align="end">
             <DropdownMenuItem asChild>
-              <Link href='/latest'>
-                <GemIcon className='size-4 opacity-75' /> Latest
+              <Link
+                className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+                href="/latest"
+              >
+                <GemIcon className="size-4 opacity-75" /> Latest
               </Link>
             </DropdownMenuItem>
             <DropdownMenuItem asChild>
-              <Link href='/categories'>
-                <BlocksIcon className='size-4 opacity-75' /> Categories
+              <Link
+                className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+                href="/categories"
+              >
+                <BlocksIcon className="size-4 opacity-75" /> Categories
               </Link>
             </DropdownMenuItem>
             <DropdownMenuItem asChild>
-              <Link href='/alternatives'>
-                <SmilePlusIcon className='size-4 opacity-75' /> Alternatives
+              <Link
+                className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+                href="/alternatives"
+              >
+                <SmilePlusIcon className="size-4 opacity-75" /> Alternatives
               </Link>
             </DropdownMenuItem>
             <DropdownMenuItem asChild>
-              <Link href='/languages'>
-                <BracesIcon className='size-4 opacity-75' /> Languages
+              <Link
+                className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+                href="/languages"
+              >
+                <BracesIcon className="size-4 opacity-75" /> Languages
               </Link>
             </DropdownMenuItem>
             <DropdownMenuItem asChild>
-              <Link href='/topics'>
-                <TagIcon className='size-4 opacity-75' /> Topics
+              <Link
+                className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+                href="/topics"
+              >
+                <TagIcon className="size-4 opacity-75" /> Topics
               </Link>
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
 
-        <Link href='/about'>About</Link>
-        <Link href='/sponsor'>Sponsor</Link>
+        <Link
+          className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+          href="/about"
+        >
+          About
+        </Link>
+        <Link
+          className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+          href="/sponsor"
+        >
+          Sponsor
+        </Link>
       </nav>
-      {/* 
-      <Series size='sm'>
-        <ClientOnly>{() => <ThemeSwitcher />}</ClientOnly>
+
+      <Series size="sm">
+        {/* <ClientOnly>{() => <ThemeSwitcher />}</ClientOnly> */}
 
         <Button
-          size='sm'
-          variant='secondary'
+          size="sm"
+          variant="secondary"
           prefix={<GithubIcon />}
           suffix={
-            <>
-              {!error && (
-                <Badge size='sm' className='-my-0.5 size-auto'>
-                  {isLoading && <LoaderIcon className='size-3 animate-spin' />}
-                  {data && formatter.format(data)}
-                </Badge>
-              )}
-            </>
+            <Badge
+              size="sm"
+              className="-my-0.5 size-auto px-1 py-px text-[10px]/tight"
+            >
+              {/* {isLoading && <LoaderIcon className="size-3 animate-spin" />}
+              {data && formatter.format(data)} */}
+              439
+            </Badge>
           }
           asChild
         >
-          <a href={GITHUB_URL} target='_blank' rel='nofollow noreferrer'>
+          <a href={GITHUB_URL} target="_blank" rel="nofollow noreferrer">
             Star
-            <Ping className='absolute -top-1 -right-1' />
+            <Ping className="absolute -right-1 -top-1" />
           </a>
         </Button>
 
         <Button
-          size='sm'
-          variant='secondary'
+          size="sm"
+          variant="secondary"
           prefix={<PlusIcon />}
-          className='-my-1.5 max-sm:hidden'
+          className="-my-1.5 max-sm:hidden"
           asChild
         >
-          <NavLink to='/submit' unstable_viewTransition>
+          <Link
+            className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+            href="/submit"
+          >
             Submit
-          </NavLink>
+          </Link>
         </Button>
       </Series>
 
       {isNavOpen && (
-        <nav className='mt-2 flex flex-col gap-y-2 w-full lg:hidden'>
-          <Link href='/latest'>Latest</NavigationLink>
-          <Link href='/categories'>Categories</NavigationLink>
-          <NavigationLink to='/alternatives'>Alternatives</NavigationLink>
-          <NavigationLink to='/languages'>Languages</NavigationLink>
-          <NavigationLink to='/topics'>Topics</NavigationLink>
-          <NavigationLink to='/submit'>Submit</NavigationLink>
-          <NavigationLink to='/sponsor'>Sponsor</NavigationLink>
-          <NavigationLink to='/about'>About</NavigationLink>
+        <nav className="mt-2 flex w-full flex-col gap-y-2 lg:hidden">
+          <Link
+            className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+            href="/latest"
+          >
+            Latest
+          </Link>
+          <Link
+            className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+            href="/categories"
+          >
+            Categories
+          </Link>
+          <Link
+            className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+            href="/alternatives"
+          >
+            Alternatives
+          </Link>
+          <Link
+            className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+            href="/languages"
+          >
+            Languages
+          </Link>
+          <Link
+            className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+            href="/topics"
+          >
+            Topics
+          </Link>
+          <Link
+            className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+            href="/submit"
+          >
+            Submit
+          </Link>
+          <Link
+            className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+            href="/sponsor"
+          >
+            Sponsor
+          </Link>
+          <Link
+            className="group -m-0.5 flex cursor-pointer items-center gap-2 p-0.5 text-sm -tracking-micro text-muted hover:text-foreground disabled:opacity-50"
+            href="/about"
+          >
+            About
+          </Link>
         </nav>
-      )} */}
+      )}
     </div>
-  )
-}
+  );
+};
