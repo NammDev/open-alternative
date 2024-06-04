b104cefa10721927bb7fc29d8d7c4a41cdb47b28
Nam <namdeveloper.ca@gmail.com>
2024-06-04 17:25:05 +0700

    github get star

---
+++ src/app/api/fetch-repository-stars/route.ts
@@ -0,0 +1,31 @@
+import { z } from "zod";
+import { graphql } from "@octokit/graphql";
+import { repositoryStarsQuery, RepositoryStarsQueryResult } from "@/lib/github";
+
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const searchParams = Object.fromEntries(url.searchParams);
+
+  const schema = z.object({
+    owner: z.string(),
+    name: z.string(),
+  });
+
+  const { owner, name } = schema.parse(searchParams);
+
+  try {
+    const { repository } = await graphql<RepositoryStarsQueryResult>({
+      query: repositoryStarsQuery,
+      owner,
+      name,
+      headers: { authorization: `token ${process.env.GITHUB_TOKEN}` },
+    });
+
+    return new Response(repository.stargazerCount.toString(), {
+      status: 200,
+    });
+  } catch (error) {
+    console.error(`Failed to fetch repository stars ${owner}/${name}`, error);
+    throw new Error(`Failed to fetch repository stars ${owner}/${name}`);
+  }
+}
--- src/lib/github.ts
+++ src/lib/github.ts
@@ -32,3 +32,109 @@ export async function getGithubStars() {
     },
   )();
 }
+
+import { DAY_IN_MS } from "./constants";
+
+export type RepositoryQueryResult = {
+  repository: {
+    stargazerCount: number;
+    forkCount: number;
+    watchers: {
+      totalCount: number;
+    };
+    mentionableUsers: {
+      totalCount: number;
+    };
+    licenseInfo: {
+      spdxId: string;
+    };
+    defaultBranchRef: {
+      target: {
+        history: {
+          edges: Array<{
+            node: {
+              committedDate: string;
+            };
+          }>;
+        };
+      };
+    };
+    repositoryTopics: {
+      nodes: {
+        topic: {
+          name: string;
+        };
+      }[];
+    };
+    languages: {
+      totalSize: number;
+      edges: Array<{
+        size: number;
+        node: {
+          name: string;
+          color: string;
+        };
+      }>;
+    };
+  };
+};
+
+export type RepositoryStarsQueryResult = {
+  repository: {
+    stargazerCount: number;
+  };
+};
+
+export const repositoryQuery = `query RepositoryQuery($owner: String!, $name: String!) {
+  repository(owner: $owner, name: $name) {
+    stargazerCount
+    forkCount
+    watchers {
+      totalCount
+    }
+    mentionableUsers {
+      totalCount
+    }
+    licenseInfo {
+      spdxId
+    }
+    defaultBranchRef {
+      target {
+        ... on Commit {
+          history(first: 1) {
+            edges {
+              node {
+                ... on Commit {
+                  committedDate
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    repositoryTopics(first: 25) {
+      nodes {
+        topic {
+          name
+        }
+      }
+    }
+    languages(first: 3, orderBy: { field: SIZE, direction: DESC}) {
+      totalSize
+      edges {
+        size
+        node {
+          name
+          color
+        }
+      }
+    }
+  }
+}`;
+
+export const repositoryStarsQuery = `query RepositoryQuery($owner: String!, $name: String!) {
+  repository(owner: $owner, name: $name) {
+    stargazerCount
+  }
+}`;
